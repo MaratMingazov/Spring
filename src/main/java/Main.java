53f574d72ae02e8298fb727044cf0594a60c3908
MaratMingazov <maratmingazovr@gmail.com>
2017-03-18 10:01:06 +0300

    Add new structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
-package main;
-
 /**
- * Created by maratmingazovr on 15.03.17.
+ * Created by maratmingazovr on 18.03.17.
  */
 public class Main {
     public static void main(String[] args) {
-        
+        System.err.println("Start!");
     }
 }
