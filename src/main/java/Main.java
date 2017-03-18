ff6cd9071c6066b722d9eec1727fd7b3cf1b1bc8
MaratMingazov <maratmingazovr@gmail.com>
2017-03-18 10:21:46 +0300

    Add Spring context and Spring core

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
-/**
- * Created by maratmingazovr on 18.03.17.
- */
+
+
+
 public class Main {
     public static void main(String[] args) {
         System.err.println("Start!");
---
+++ src/main/java/Person.java
@@ -0,0 +1,5 @@
+/**
+ * Created by maratmingazovr on 18.03.17.
+ */
+public class Person {
+}
