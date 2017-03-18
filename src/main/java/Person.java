46be2c166617f9be2dfec184f8b32938d325cf5a
MaratMingazov <maratmingazovr@gmail.com>
2017-03-18 12:15:32 +0300

    Add application-context.xml

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,41 @@
-/**
- * Created by maratmingazovr on 18.03.17.
- */
+
 public class Person {
+
+    private final long id;
+    private String name;
+    private String email;
+    private int age;
+
+    public Person(final int pId) {
+        this.id = pId;
+    }
+
+    public final long getId() {
+        return id;
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    public final void setName(final String pName) {
+        this.name = pName;
+    }
+
+    public final String getEmail() {
+        return email;
+    }
+
+    public final void setEmail(final String pEmail) {
+        this.email = pEmail;
+    }
+
+    public final int getAge() {
+        return age;
+    }
+
+    public final void setAge(final int pAge) {
+        this.age = pAge;
+    }
+
 }
