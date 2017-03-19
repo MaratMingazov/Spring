9e438fa94f0c86f52c16e16afe44065b1ada3cb1
MaratMingazov <maratmingazovr@gmail.com>
2017-03-19 16:23:40 +0300

    Correct Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 
+import org.library.LibraryService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.err.println("Start!");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+        LibraryService libraryService = (LibraryService) context.getBean("LibraryService");
+
+        libraryService.addBook("Author1", "title1");
+        libraryService.addBook("Author2", "title2");
+
+        System.err.println(libraryService.getBooksList());
     }
 }
